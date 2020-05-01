72531e370b6fecc69dc1cb2876b3c30e0f82438f
MJ Harvey <m.j.harvey@acellera.com>
2020-05-01 16:57:15 +0100

    gpfs build

--- nodetype/cx2/20_setup_gpfs.sh
+++ nodetype/cx2/20_setup_gpfs.sh
@@ -1,6 +1,14 @@
 # Install route to GPFS network
 ip route add 172.16.0.0/255.255.255.192 via 10.148.0.2
 
+# Install the RPMs
+rpm -ivh /usr/local/gpfs/*rpm
+
+# Fixup th symlink
+rm /rds/general
+rm /rds
+ln -s /rdsgpfs /rds
+
 ##Build GPFS GPL
 /usr/lpp/mmfs/bin/mmbuildgpl
 /usr/lpp/mmfs/bin/mmstartup
