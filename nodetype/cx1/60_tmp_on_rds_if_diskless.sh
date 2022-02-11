31e122f785c459c9c8c0f212caccc62e337ca26b
Bob Cregan <b.cregan@imperial.ac.uk>
2022-02-11 09:47:53 +0000

    Old tmpdir scripts gone

--- nodetype/cx1/60_tmp_on_rds_if_diskless.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# MJH 4/9/2019 -- if the / is a /tmpfs, bind mount RDS ephemeral/tmpdir onto /tmp
-mount | grep -q "tmpfs on / "
-if [ "$?" == "0" ]; then
-	mount -o bind /rds/general/ephemeral/tmpdir /tmp
-fi
-
-
--- nodetype/cx1/65_make_tmp_readonly.sh
+++
@@ -1,5 +0,0 @@
-# === Restrict /tmp to be read-only ===
-
-# Now restrict /tmp and /var/tmp to prevent users writing in it
-chmod 0755 /tmp /var/tmp
-
