d7b348f30c54f9147a5a7bde8560403ac7c8f689
veioenza <cyber_veio@hotmail.com>
2022-03-14 10:54:08 +0000

    Using arbiter for cgroups on cx3 login nodes

---
+++ nodetype/login/90_arbiter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Only setup to run on RHEL 8 login nodes
+if [ $(grep "REDHAT_SUPPORT_PRODUCT_VERSION" /etc/os-release | grep -o 8) -eq 8 ] ; then
+                                mkdir -p /opt/arbiter2-2.0.0/logs/`hostname`/plots
+                                chown -R arbiter: /opt/arbiter2-2.0.0/
+                                chmod 770 /opt/arbiter2-2.0.0/logs
+                                systemctl --now enable arbiter2
+                                cd /opt/arbiter2-2.0.0/tools
+                                ./allusers_corraller.sh
+fi
+
