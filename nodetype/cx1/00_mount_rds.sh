9caed6903ac56ddb76058748ef619e30cfe73458
veioenza <cyber_veio@hotmail.com>
2022-05-12 14:24:27 +0100

    Adding mount of easybuild fileset

--- nodetype/cx1/00_mount_rds.sh
+++ nodetype/cx1/00_mount_rds.sh
@@ -4,10 +4,12 @@ RDS=hpc.rds.ic.ac.uk
 
 mkdir /rds
 mkdir /rds/general
+mkdir /rds/easybuild
+chmod a+rw /rds/easybuild
 chmod a+rx /rds/general
 chmod a+rx /rds
 ln -s /rds /rdsgpfs
 
 # not noac, too many stat-heavy things run on cx1 nodes
 mount -t nfs -o mountvers=3,nfsvers=3,nodev,nosuid,rsize=65536,wsize=65536 $RDS:/rds/general /rds/general 
-
+mount -t nfs -o mountvers=3,nfsvers=3,nodev,nosuid,rsize=65536,wsize=65536 $RDS:/rds/easybuild /rds/easybuild/
