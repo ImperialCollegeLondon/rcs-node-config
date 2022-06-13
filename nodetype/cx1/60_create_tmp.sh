2183f63e7de6b5b8c43d9f6ebecfc3baf7011a34
veioenza <cyber_veio@hotmail.com>
2022-06-13 08:15:58 +0100

    Adding format of /tmp

---
+++ nodetype/cx1/60_create_tmp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# If there is another partition then assume it is to be used as a local /tmp
+
+mount_tmp (){
+  unlink /var/tmp
+  mv /tmp /var/tmp
+  chmod 755 /var/tmp
+  mkdir /tmp
+  # Remember stick bit so users can delete tmp by mistake
+  mount ${1} /tmp
+  chmod 1777 /tmp
+}
+
+if grep -q nvme0n1p2 /proc/partitions  ; then
+  mkfs.xfs /dev/nvme0n1p2 
+  mount_tmp /dev/nvme0n1p2
+elif grep -q sda2 /proc/partitions ; then
+  mkfs.xfs -f /dev/sda2
+  mount_tmp /dev/sda2
+else
+  chmod 755 /tmp
+fi
