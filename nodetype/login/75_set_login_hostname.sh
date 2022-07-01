56cf0e0e264afc7affbd61ec2ffad8d645d86811
veioenza <cyber_veio@hotmail.com>
2022-07-01 10:44:53 +0100

    Added check for login-dev and removed mounting rds for /tmp

--- nodetype/login/75_set_login_hostname.sh
+++ nodetype/login/75_set_login_hostname.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-for i in {a..z};do
+## We assume that eth0 is the interface that the DNS is set on. 
+IPv6=$(ip -6 addr show dev eth0 | egrep -o "2a0c:[a-z0-9:]*")
 
-	## We assume that eth0 is the interface that the DNS is set on
-	## get the IP
-	V6IP=`ip -6 addr show dev eth0 | grep inet6 | awk '{print $2}' | grep 2a0c | sed s/\\\/64//`
-
-	CUURENTIP=`/usr/bin/dig login-${i}.hpc.ic.ac.uk AAAA +short`
-
-	if [ "${CUURENTIP}" == "${V6IP}" ];then
-
-		hostname login-${i}
-		break 
-
-	fi 
-
+#Because configuration expects cx3 to follow a given pattern, any login-x address has to be an alias and so we have to look for it.
+for i in dev {a..z};do
+    TEST_IP=`/usr/bin/dig login-${i}.hpc.ic.ac.uk AAAA +short`
+    if [ "${CUURENTIP}" == "${V6IP}" ];then
+        hostname login-${i}
+        break 
+    fi 
 done
