4e5b133425a615948a691e9a419afcfc4dbb3f1c
veioenza <cyber_veio@hotmail.com>
2022-07-26 11:18:02 +0100

    Removed dev for now as it returns NULL

--- nodetype/login/75_set_login_hostname.sh
+++ nodetype/login/75_set_login_hostname.sh
@@ -4,7 +4,7 @@
 IPv6=$(ip -6 addr show dev eth0 | egrep -o "2a0c:[a-z0-9:]*")
 
 #Because configuration expects cx3 to follow a given pattern, any login-x address has to be an alias and so we have to look for it.
-for i in dev {a..z};do
+for i in {a..z};do
     TEST_IP=`/usr/bin/dig login-${i}.hpc.ic.ac.uk AAAA +short`
     if [ "${CUURENTIP}" == "${V6IP}" ];then
         hostname login-${i}
