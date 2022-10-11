de436528f7fa86714a94e72c71e4782519947de7
David Hempston <cyber_veio@hotmail.com>
2022-10-11 13:49:50 +0100

    Removed pbs.conf changes for login-a

--- hostname/login-a/80_multihomed_pbs.sh
+++ hostname/login-a/80_multihomed_pbs.sh
@@ -13,12 +13,10 @@ if [ "$(hostname -s)" != "${AUTHORISED_HOST}" ]; then
   exit 1
 fi
 
-##Do pbs.conf
-sed -i 's/PBS_SERVER=pbs-cx3/PBS_SERVER=pbs/g' /etc/pbs.conf
 
-##Then Hosts
+##Hosts
 sed -i 's/##192.168.101.250 pbs1 pbs/192.168.101.250 pbs1 pbs/g' /etc/hosts
-sed -i 's/10.149.16.100   pbs-cx3 pbs/10.149.16.100   pbs-cx3/g' /etc/hosts
+sed -i 's/10.149.16.100  pbs pbs-cx3/10.149.16.100  pbs-cx3/g' /etc/hosts
 
 #Put sssd back on track
 sed -i 's/ipv6_only/ipv4_only/g' /etc/sssd/sssd.conf
