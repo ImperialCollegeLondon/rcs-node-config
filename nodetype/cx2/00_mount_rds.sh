c23e449a7c4f660e009b4e243da9dcfbee5523bf
David Hempston <cyber_veio@hotmail.com>
2022-10-12 14:31:51 +0100

    Check for RDS mount on cx2

--- nodetype/cx2/00_mount_rds.sh
+++ nodetype/cx2/00_mount_rds.sh
@@ -1,12 +1,20 @@
 
 
-RDS=hpc-ib.rds.ic.ac.uk
+RDS=hpc.rds.ic.ac.uk
 
 mkdir /rds
 mkdir /rds/general
-chmod a+rx /rds
+mkdir /rds/easybuild
+chmod a+rw /rds/easybuild
 chmod a+rx /rds/general
+chmod a+rx /rds
 ln -s /rds /rdsgpfs
 
-mount -t nfs -o sync,noac,mountvers=3,nfsvers=3,nodev,nosuid,rsize=1048576,wsize=1048576 $RDS:/rds/general /rds/general 
-
+# not noac, too many stat-heavy things run on cx1 nodes
+# Using if statements to prevent double mounting.
+if ! df  | grep -q "hpc.rds.ic.ac.uk:/rds/general"; then
+  mount -t nfs -o sync,noac,mountvers=3,nfsvers=3,nodev,nosuid,rsize=1048576,wsize=1048576 $RDS:/rds/general /rds/general
+fi
+if ! df  | grep -q "hpc.rds.ic.ac.uk:/rds/easybuild"; then
+  mount -t nfs -o sync,noac,mountvers=3,nfsvers=3,nodev,nosuid,rsize=1048576,wsize=1048576 $RDS:/rds/easybuild /rds/easybuild/
+fi
