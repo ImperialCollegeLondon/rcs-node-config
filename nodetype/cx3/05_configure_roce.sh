a5dfbbb33444fbadbec63ae22823ea1438462e63
David Hempston <cyber_veio@hotmail.com>
2022-10-12 15:22:04 +0100

    ROCE setup url using RHEL 8

--- nodetype/cx3/05_configure_roce.sh
+++ nodetype/cx3/05_configure_roce.sh
@@ -3,9 +3,10 @@
 ## RoCE setup
 # https://community.mellanox.com/s/article/lossless-roce-configuration-for-linux-drivers-in-dscp-based-qos-mode
 # https://community.mellanox.com/s/article/howto-configure-dcqcn--roce-cc--values-for-connectx-4--linux-x
-#The above don't work any more
+# The above don't work any more
 # https://mymellanox.force.com/mellanoxcommunity/s/article/lossless-roce-configuration-for-linux-drivers-in-dscp-based-qos-mode
-
+# Just using RHEL drives.
+# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/configuring_infiniband_and_rdma_networks/configuring-roce_configuring-infiniband-and-rdma-networks
 mkdir /sys/kernel/config/rdma_cm/mlx5_0
 mlnx_qos -i eth0 --trust dscp
 # Enable PFC on priority 3
