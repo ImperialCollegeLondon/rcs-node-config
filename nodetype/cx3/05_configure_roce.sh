3463f97f44309c44cc09fd96845f1101a8a4392b
David Hempston <cyber_veio@hotmail.com>
2022-10-12 15:20:07 +0100

    Added ROCE url for ref

--- nodetype/cx3/05_configure_roce.sh
+++ nodetype/cx3/05_configure_roce.sh
@@ -3,6 +3,9 @@
 ## RoCE setup
 # https://community.mellanox.com/s/article/lossless-roce-configuration-for-linux-drivers-in-dscp-based-qos-mode
 # https://community.mellanox.com/s/article/howto-configure-dcqcn--roce-cc--values-for-connectx-4--linux-x
+#The above don't work any more
+# https://mymellanox.force.com/mellanoxcommunity/s/article/lossless-roce-configuration-for-linux-drivers-in-dscp-based-qos-mode
+
 mkdir /sys/kernel/config/rdma_cm/mlx5_0
 mlnx_qos -i eth0 --trust dscp
 # Enable PFC on priority 3
