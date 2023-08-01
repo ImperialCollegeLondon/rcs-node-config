66c135ec03bc0b0567ef988d49b63d990bde135b
David Hempston <cyber_veio@hotmail.com>
2023-08-01 08:51:05 +0100

    Missing slash

--- hostname/cx3-2-29/99_auto_updates.sh
+++ hostname/cx3-2-29/99_auto_updates.sh
@@ -22,7 +22,7 @@ sed -i "s/apply_updates = no/apply_updates = yes/g" /etc/dnf/automatic.conf
 echo "exclude=kernel* redhat-release* kmod-*" >> /etc/yum.conf
 
 # Run updates on a Tuesday morning
-cat > usr/lib/systemd/system/dnf-automatic.timer << EOF
+cat > /usr/lib/systemd/system/dnf-automatic.timer << EOF
 [Unit]
 Description=rcs-dnf-automatic timer
 # See comment in dnf-makecache.service
