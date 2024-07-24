937128173012aee38e807266dcd5ec55441654ab
David Hempston <cyber_veio@hotmail.com>
2024-07-24 10:40:10 +0000

    enable Cron job in OOD and Jupyter compute nodes

---
+++ hostname/cx3-4-29/20_cgroup_cron.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Only run on Interactive nodes like OOD
+echo "*/10 * * * * root /opt/pbs/custom/cgroups-limits.sh" > /etc/cron.d/simple-cgroups.sh
+chmod 644 /etc/cron.d/simple-cgroups.sh
