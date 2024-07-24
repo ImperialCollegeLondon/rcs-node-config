8e47788219cc4db6e22f413f43fe7eb98e12e08a
David Hempston <cyber_veio@hotmail.com>
2024-07-24 10:52:37 +0000

    Install missing packages

--- hostname/cx3-1-29/20_cgroup_cron.sh
+++ hostname/cx3-1-29/20_cgroup_cron.sh
@@ -2,3 +2,4 @@
 # Only run on Interactive nodes like OOD
 echo "*/10 * * * * root /opt/pbs/custom/cgroups-limits.sh" > /etc/cron.d/simple-cgroups.sh
 chmod 644 /etc/cron.d/simple-cgroups.sh
+dnf -y install libcgroup-tools
